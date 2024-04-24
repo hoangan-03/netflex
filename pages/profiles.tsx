8141d8f7255f815215e5082c9844e981ffa4d412
John Nguyen <john12052003@gmail.com>
2024-04-25 02:09:17 +0700

    /

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -25,7 +25,7 @@ const Profiles = () => {
   return (
     <div className='w-[50%] h-[30%] flex netflix text-bold flex-col justify-center gap-10 items-center absolute top-[25%] left-[25%]'>
         
-        <p className='text-white text-6xl'>Who's watching?</p>
+        <p className='text-white text-6xl'>Who&apos;s watching?</p>
         <div className='w-full h-auto flex gap-8 flex-row justify-center items-center'>
             <div className='w-1/5 h-full flex flex-col gap-4 '>
                 <Image src ={profile} alt =''className='w-full h-auto aspect-square bg-black rounded-lg cursor-pointer hover:border-4 border-white ' onClick={() => router.push('/')}></Image>

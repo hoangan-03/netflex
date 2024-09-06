4cdf8ddc63854197f6638aa7f05980f0ec800e1b
John Nguyen <john12052003@gmail.com>
2024-09-07 02:20:05 +0700

    test

--- hooks/useMovie.ts
+++ hooks/useMovie.ts
@@ -1,6 +1,6 @@
 import useSwr from 'swr'
 import fetcher from '@/lib/fetcher';
-const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+const baseUrl = process.env.NEXTAUTH_URL;
 const useMovie = (videoID?: string) => {
   const { data, error, isLoading } = useSwr(videoID ? `${baseUrl}/api/movies/${videoID}` : null, fetcher, {
     revalidateIfStale: false,
--- hooks/useMovieList.ts
+++ hooks/useMovieList.ts
@@ -1,6 +1,6 @@
 import useSwr from 'swr'
 import fetcher from '@/lib/fetcher';
-const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+const baseUrl = process.env.NEXTAUTH_URL;
 const useMovies = () => {
   const { data, error, isLoading } = useSwr(`${baseUrl}/api/movies`, fetcher, {
     revalidateIfStale: false,

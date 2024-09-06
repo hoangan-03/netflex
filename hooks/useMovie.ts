1d65f38318e95e30775076248e40243e26f62242
John Nguyen <john12052003@gmail.com>
2024-09-07 02:18:39 +0700

    test

--- hooks/useMovie.ts
+++ hooks/useMovie.ts
@@ -1,8 +1,8 @@
 import useSwr from 'swr'
 import fetcher from '@/lib/fetcher';
-
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 const useMovie = (videoID?: string) => {
-  const { data, error, isLoading } = useSwr(videoID ? `/api/movies/${videoID}` : null, fetcher, {
+  const { data, error, isLoading } = useSwr(videoID ? `${baseUrl}/api/movies/${videoID}` : null, fetcher, {
     revalidateIfStale: false,
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
--- hooks/useMovieList.ts
+++ hooks/useMovieList.ts
@@ -1,8 +1,8 @@
 import useSwr from 'swr'
 import fetcher from '@/lib/fetcher';
-
+const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
 const useMovies = () => {
-  const { data, error, isLoading } = useSwr('/api/movies', fetcher, {
+  const { data, error, isLoading } = useSwr(`${baseUrl}/api/movies`, fetcher, {
     revalidateIfStale: false,
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
